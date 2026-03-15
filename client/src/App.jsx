cf56c9bc679e9b76d4fbd32d1ab58fd0e5b4617c
Simscope <sandrvlad@gmail.com>
2026-03-15 14:34:15 -0400

    Update routing in App component with new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import DeviceMonitoringPage from './pages/DeviceMonitoringPage'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MonitoringPage from './pages/MonitoringPage'
+import MonitoringNJPage from './pages/MonitoringNJPage'
+import MonitoringPAPage from './pages/MonitoringPAPage'
+import AdminPage from './pages/AdminPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DeviceMonitoringPage />} />
-        <Route path="/monitoring" element={<DeviceMonitoringPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/monitoring" element={<MonitoringPage />} />
+        <Route path="/monitoring/nj" element={<MonitoringNJPage />} />
+        <Route path="/monitoring/pa" element={<MonitoringPAPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
