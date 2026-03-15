32bab229fc15b25498c0bf00700afe6cdf93b3de
Simscope <sandrvlad@gmail.com>
2026-03-15 15:50:13 -0400

    Wrap MonitoringPage in BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import MonitoringPage from './pages/MonitoringPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<MonitoringPage/>
+  <BrowserRouter>
+    <MonitoringPage />
+  </BrowserRouter>
 )
