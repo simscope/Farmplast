a4de4cec4802d8b680202f59f2ff3f2acb5d3434
Simscope <sandrvlad@gmail.com>
2026-03-15 15:54:36 -0400

    Remove BrowserRouter wrapper from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MonitoringPage from './pages/MonitoringPage'
 import MonitoringNJPage from './pages/MonitoringNJPage'
@@ -8,15 +7,12 @@ import AdminPage from './pages/AdminPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/monitoring" element={<MonitoringPage />} />
-        <Route path="/monitoring/nj" element={<MonitoringNJPage />} />
-        <Route path="/monitoring/pa" element={<MonitoringPAPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/monitoring" element={<MonitoringPage />} />
+      <Route path="/monitoring/nj" element={<MonitoringNJPage />} />
+      <Route path="/monitoring/pa" element={<MonitoringPAPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
   )
 }
