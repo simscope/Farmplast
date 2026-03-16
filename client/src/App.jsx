637be844c46dad2b8b8af0cc2bd7a7007c15f0fe
Simscope <sandrvlad@gmail.com>
2026-03-16 01:04:14 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { supabase } from '../lib/supabase'
+import { supabase } from './lib/supabase.js'
 
 export default function LoginPage() {
   const navigate = useNavigate()
