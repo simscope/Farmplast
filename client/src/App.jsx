ae0565713f45f37d295d0a613958b40e0ec3c8c5
Simscope <sandrvlad@gmail.com>
2026-03-16 01:15:03 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,106 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { AuthProvider } from './context/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
+
+import ProtectedRoute from './components/ProtectedRoute'
 
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import MonitoringPage from './pages/MonitoringPage'
+import MonitoringNJPage from './pages/MonitoringNJPage'
+import MonitoringPAPage from './pages/MonitoringPAPage'
 import AdminPage from './pages/AdminPage'
 import AccountingPage from './pages/AccountingPage'
 import EmployeesPage from './pages/EmployeesPage'
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
 
-        <Route path="/monitoring" element={<MonitoringPage />} />
-        <Route path="/monitoring/:location" element={<MonitoringPage />} />
+          {/* Public */}
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminPage />
-            </ProtectedRoute>
-          }
-        />
+          {/* Dashboard */}
 
-        <Route
-          path="/accounting"
-          element={
-            <ProtectedRoute>
-              <AccountingPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/employees"
-          element={
-            <ProtectedRoute>
-              <EmployeesPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </AuthProvider>
+          {/* Monitoring */}
+
+          <Route
+            path="/monitoring"
+            element={
+              <ProtectedRoute>
+                <MonitoringPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/monitoring/nj"
+            element={
+              <ProtectedRoute>
+                <MonitoringNJPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/monitoring/pa"
+            element={
+              <ProtectedRoute>
+                <MonitoringPAPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Admin */}
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute roles={['admin']}>
+                <AdminPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Accounting */}
+
+          <Route
+            path="/accounting"
+            element={
+              <ProtectedRoute roles={['admin', 'accountant']}>
+                <AccountingPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Employees */}
+
+          <Route
+            path="/employees"
+            element={
+              <ProtectedRoute roles={['admin']}>
+                <EmployeesPage />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
