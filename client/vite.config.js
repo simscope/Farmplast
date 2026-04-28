c967436d44dcde24b48c374b2990330e2eb4f149
Simscope <sandrvlad@gmail.com>
2026-04-27 21:27:25 -0400

    Add proxy configuration to Vite server

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,3 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
+server: {
+  proxy: {
+    '/zkt': {
+      target: 'http://127.0.0.1:8787',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/zkt/, ''),
+    },
+  },
+},
